e8f606b8be48885980522bf42d4e445ceb0884a4
享韵 <yuanqiuyan@yuanqiuyantekiMacBook-Air.local>
2016-10-01 18:16:50 +0800

    first commit

---
+++ Common/ADXMacro.h
@@ -0,0 +1,43 @@
+//
+//  ADXMacro.h
+//  ADX
+//
+//  Created by MDJ on 2016/10/1.
+//  Copyright © 2016年 mdj. All rights reserved.
+//
+
+#ifndef ADXMacro_h
+#define ADXMacro_h
+
+
+#pragma mark Color
+
+
+
+#define UIColorRGB(r, g, b)     [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1.0f]
+#define UIColorRGBA(r, g, b, a) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)/255.0]
+#define UIColorHSL(h, s, l)     [UIColor colorWithHue:(h)/255.0f saturation:(s)/255.0f brightness:(l)/255.0f alpha:1.0f]
+#define UIColorHSLA(h, s, l, a) [UIColor colorWithHue:(h)/255.0f saturation:(s)/255.0f brightness:(l)/255.0f alpha:(a)/255.0f]
+
+
+
+
+#pragma mark NSLog
+
+
+
+#ifdef DEBUG
+#define MDJLog(format, ...) do {                                             \
+fprintf(stderr, "<%s : %d> %s\n",                                           \
+[[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String],  \
+__LINE__, __func__);                                                        \
+(NSLog)((format), ##__VA_ARGS__);                                           \
+fprintf(stderr, "-------\n");                                               \
+} while (0)
+#else
+#define NSLog(format, ...)
+#endif
+
+
+
+#endif /* ADXMacro_h */
---
+++ Common/Constant.h
@@ -0,0 +1,23 @@
+//
+//  Constant.h
+//  ADX
+//
+//  Created by yuanqiuyan on 2016/10/1.
+//  Copyright © 2016年 mdj. All rights reserved.
+//
+
+#ifndef Constant_h
+#define Constant_h
+
+#pragma mark url
+
+//服务器地址
+#ifdef DEBUG
+#define SERVER_URL          @"http://123.57.190.60:8080/api/"
+#else
+#define SERVER_URL          @"http://123.57.190.60:8080/api/"
+#endif
+
+
+
+#endif /* Constant_h */
